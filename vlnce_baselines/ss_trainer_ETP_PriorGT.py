0cd47d0dbb5d812e5258cfb69dff39cde99d567c
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-05-11 02:03:45 +0000

    feat: wire map tokens through PriorGT

--- vlnce_baselines/ss_trainer_ETP_PriorGT.py
+++ vlnce_baselines/ss_trainer_ETP_PriorGT.py
@@ -1042,12 +1042,14 @@ class RLTrainer(BaseVLNCETrainer):
                     if cognitive_map else PrecomputedCognitiveMap.empty_start_position()
                     for cognitive_map in cognitive_maps[:self.envs.num_envs]
                 ]).to(self.device)
-                nav_inputs['map_embeds'] = self.policy.net(
+                map_tokens, map_token_masks = self.policy.net(
                     mode='map_encoding',
                     cognitive_crops=cognitive_crops,
                     direction_vectors=direction_vectors,
                     start_positions=start_positions,
                 )
+                nav_inputs['map_tokens'] = map_tokens
+                nav_inputs['map_token_masks'] = map_token_masks
 
             nav_outs = self.policy.net(**nav_inputs)
             nav_logits = nav_outs['global_logits']
