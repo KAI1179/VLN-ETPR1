085daffc27f0923e55740824722a506673185298
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-05-11 23:20:06 -0400

    fix: Pretrain

--- pretrain_src/run_pt/run_mix_server.bash
+++ pretrain_src/run_pt/run_mix_server.bash
@@ -1,38 +1,30 @@
-#!/bin/bash
-
-# Source - https://stackoverflow.com/a/56431189
-# Posted by Lyn, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-04-17, License - CC BY-SA 4.0
-
-has_param() {
-    local term="$1"
-    shift
-    for arg; do
-        if [[ $arg == "$term" ]]; then
-            return 0
-        fi
-    done
-    return 1
-}
-
 NODE_RANK=0
 NUM_GPUS=4
 PORT=$1
-if has_param '--use_prior_gt' "$@"; then
-    outdir=pretrained/r2r_rxr_ce/prior_gt
-else
-    outdir=pretrained/r2r_rxr_ce/baseline
-fi
-echo "Output dir: $outdir"
 shift
 
+outdir=pretrained/r2r_rxr_ce/baseline
+args=()
+for arg in "$@"; do
+    case "$arg" in
+        --use_prior_gt)
+            outdir=pretrained/r2r_rxr_ce/prior_gt
+            ;;
+        *)
+            args+=("$arg")
+            ;;
+    esac
+done
+
+echo "Using port: $PORT"
+echo "Output dir: $outdir"
+echo "Args: ${args[*]}"
+
 PYTHONPATH=$PYTHONPATH:. python -m torch.distributed.launch \
-    --nproc_per_node=${NUM_GPUS} --node_rank $NODE_RANK --master_port=$PORT \
-    pretrain_src/pretrain_src/train_r2r.py --world_size ${NUM_GPUS} \
+    --nproc_per_node=${NUM_GPUS} --node_rank "$NODE_RANK" --master_port="$PORT" \
+    pretrain_src/pretrain_src/train_r2r.py --world_size "${NUM_GPUS}" \
     --vlnbert cmt \
     --model_config pretrain_src/run_pt/mix_model_config_dep.json \
     --config pretrain_src/run_pt/mix_pretrain_server.json \
-    --output_dir $outdir \
-    "$@"
-
-# CUDA_VISIBLE_DEVICES=0,1,2,3 bash pretrain_src/run_pt/run_mix_server.bash 2333
+    --output_dir "$outdir" \
+    "${args[@]}"
