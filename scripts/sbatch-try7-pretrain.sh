8d5bf791d12d5815d0ae24425b7f67fcc67d19ea
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-06-19 18:44:22 +0800

    chore: Adjust ckpt

---
+++ scripts/sbatch-try7-pretrain.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --job-name=try7
+#SBATCH --gpus=4
+#SBATCH -p vip_gpu_scze096
+eval "$(conda shell.bash hook)" && conda activate etpr1-uv
+bash pretrain_src/run_pt/run_mix_server.bash 2333 --use_prior_gt --checkpoint pretrained/r2r_rxr_ce/baseline/ckpts/model_step_367500.pt # --n_workers 4
