b9072de2a114fbf9dfea92a7dbf9cc5c640e68f6
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-06-24 18:50:45 +0800

    chore: Make scripts executable

--- scripts/sbatch-nav-pretrain.sh
+++ scripts/sbatch-nav-pretrain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=nav-pretrain
-#SBATCH --gpus=8
+#SBATCH --gpus=4
 #SBATCH -p vip_gpu_scze096
 eval "$(conda shell.bash hook)" && conda activate etpr1-uv
 bash pretrain_src/run_pt/run_mix_server.bash 2334 --use_llm --n_workers 4
