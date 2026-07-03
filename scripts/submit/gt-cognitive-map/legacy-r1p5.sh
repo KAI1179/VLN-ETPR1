2c8c280224e6f72ae84bbe331b7b01e41048c330
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-07-03 10:30:07 +0000

    Revert "chore: Submission scripts for cog map gen"
    This reverts commit 4154ef1e7115824e1f88769ca94a6096d66f6fcf.

--- scripts/submit/gt-cognitive-map/legacy-r1p5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=gt-cognitive-map-legacy-r1p5
-#SBATCH --gpus=1
-#SBATCH -p vip_gpu_scze096
-eval "$(conda shell.bash hook)" && conda activate etpr1-uv
-python -m prior.etp_r1 --map-source legacy --radius-m 1.5
-python -m prior --map-source legacy --radius-m 1.5
