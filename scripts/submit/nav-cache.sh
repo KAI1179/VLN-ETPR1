2bd373df2af6a8831e29217cb1f970789133109e
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-07-13 12:27:53 +0800

    chore: Adjust try5-like dagger ckpt; name nav-cache directories properly

--- scripts/submit/nav-cache.sh
+++ scripts/submit/nav-cache.sh
@@ -5,7 +5,7 @@
 #SBATCH -p vip_gpu_scze096
 eval "$(conda shell.bash hook)" && conda activate etpr1-uv
 python -m vlnce_baselines.models.etp_llm.generate_navigation_cache \
-  --model-name-or-path ./data/logs/llm/checkpoints/final/ \
-  --cache-model-key llama-3.1-8b-instruct \
+  --model-name-or-path ./data/logs/llm-boxes-r1p5-path5/checkpoints/final/ \
+  --cache-model-key llm-boxes-r1p5-path5 \
   --batch-size 8
 
