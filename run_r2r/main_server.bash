01d27964fb165c151815ea9f66c58820a7c76b7d
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-07-15 09:36:55 +0800

    chore: Adjust try5-like ckpt

--- run_r2r/main_server.bash
+++ run_r2r/main_server.bash
@@ -37,8 +37,8 @@ PRIOR_GT_CURRENT_GRPO_CKPT="data/logs/checkpoints/release_r2r_prior_gt_current_g
 PRIOR_GT_CURRENT_COGNITIVE_MAP_NAMESPACE="gt.bbox.r1p5.path5.v1"
 
 PRIORGT_TRY5_BLURRED_COGNITIVE_MAP_NAMESPACE="gt.legacy.r1p5.direction5.blurred.v1"
-PRIORGT_TRY5_BLURRED_PRETRAINED_CKPT="pretrained/r2r_rxr_ce/prior_gt_try5_r1p5_blurred/ckpts/model_step_425000.pt"
-PRIORGT_TRY5_BLURRED_DAGGER_CKPT="data/logs/checkpoints/release_r2r_prior_gt_try5_blurred_dagger/store/ckpt.iter30000.pth"
+PRIORGT_TRY5_BLURRED_PRETRAINED_CKPT="pretrained/r2r_rxr_ce/prior_gt_try5_r1p5_blurred/store2/try5-r1p5-blurred_step_425000.pt"
+PRIORGT_TRY5_BLURRED_DAGGER_CKPT="data/logs/checkpoints/release_r2r_prior_gt_try5_blurred_dagger/store/try5-like-blur-r1p5.iter29800.pth"
 PRIORGT_TRY5_BLURRED_GRPO_CKPT="data/logs/checkpoints/release_r2r_prior_gt_try5_blurred_grpo/store/ckpt.iter500.pth"
 
 IMAGINED_PREDICTOR_CKPT="${IMAGINED_PREDICTOR_CKPT:-}"
