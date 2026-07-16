b6835502732b42a48904ef605dfdacde9b620083
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-07-16 20:18:12 +0800

    chore: Adjust llm-grid try5 pretrain ckpt

--- run_r2r/main_server.bash
+++ run_r2r/main_server.bash
@@ -51,7 +51,7 @@ LLM_BOXES_CURRENT_DAGGER_CKPT="data/logs/checkpoints/release_r2r_llm_boxes_curre
 LLM_BOXES_CURRENT_GRPO_CKPT="data/logs/checkpoints/release_r2r_llm_boxes_current_grpo/store/ckpt.iter500.pth"
 
 LLM_GRID_TRY5_MODEL_KEY="llm-grid-r2r-legacy-r1p5-direction5-scale2"
-LLM_GRID_TRY5_PRETRAINED_CKPT="pretrained/r2r_rxr_ce/llm_grid_try5/ckpts/model_step_425000.pt"
+LLM_GRID_TRY5_PRETRAINED_CKPT="pretrained/r2r_rxr_ce/llm_grid_try5/store2/model_step_365000.pt"
 LLM_GRID_TRY5_DAGGER_CKPT="data/logs/checkpoints/release_r2r_llm_grid_try5_dagger/store/ckpt.iter30000.pth"
 LLM_GRID_TRY5_GRPO_CKPT="data/logs/checkpoints/release_r2r_llm_grid_try5_grpo/store/ckpt.iter500.pth"
 
